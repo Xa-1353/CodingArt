46967d6ec4ebcbc40b2bdb7fa4fb371c2d15cbe8
Xaria A <xaria.shella@gmail.com>
2023-08-30 10:35:22 -0500

    art

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\t \t_                        ");
+        System.out.println("\t \t\\`*-.                   ");
+        System.out.println("\t    )  _`-.                ");
+        System.out.println("\t   .  : `. .               ");
+        System.out.println("\t   : _   '  \\              ");
+        System.out.println("\t   ; *` _.   `*-._        ");
+        System.out.println("\t   `-.-'          `-.       ");
+        System.out.println("\t     ;       `       `.     ");
+        System.out.println("\t \t:.       .        \\    ");
+        System.out.println("\t \t. \\  .   :   .-'   .  ");
+        System.out.println("\t \t'  `+.;  ;  '      :   ");
+        System.out.println("\t \t:  '  |    ;       ;-. ");
+        System.out.println("\t \t; '   : :`-:     _.`* ;");
+        System.out.println("\t \t.*' /  .*' ; .*`- +'  `*' ");
+        System.out.println("\t \t`*-*   `*-*  `*-*'");
+    }
+}
