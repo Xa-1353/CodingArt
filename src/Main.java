d9ad9efbf552c603e061189a1cbccae9d8f5d732
Xaria A <xaria.shella@gmail.com>
2023-08-31 09:25:11 -0500

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\t \t_                        ");
-        System.out.println("\t \t\\`*-.                   ");
-        System.out.println("\t    )  _`-.                ");
-        System.out.println("\t   .  : `. .               ");
-        System.out.println("\t   : _   '  \\              ");
-        System.out.println("\t   ; *` _.   `*-._        ");
-        System.out.println("\t   `-.-'          `-.       ");
-        System.out.println("\t     ;       `       `.     ");
-        System.out.println("\t \t:.       .        \\    ");
-        System.out.println("\t \t. \\  .   :   .-'   .  ");
-        System.out.println("\t \t'  `+.;  ;  '      :   ");
-        System.out.println("\t \t:  '  |    ;       ;-. ");
+        System.out.println("\t \t_");
+        System.out.println("\t \t\\`*-.");
+        System.out.println("\t    )  _`-.");
+        System.out.println("\t   .  : `. .");
+        System.out.println("\t   : _   '  \\");
+        System.out.println("\t   ; *` _.   `*-._");
+        System.out.println("\t   `-.-'          `-.");
+        System.out.println("\t     ;       `       `.");
+        System.out.println("\t \t:.       .        \\");
+        System.out.println("\t \t. \\  .   :   .-'   .");
+        System.out.println("\t \t'  `+.;  ;  '      :");
+        System.out.println("\t \t:  '  |    ;       ;-.");
         System.out.println("\t \t; '   : :`-:     _.`* ;");
         System.out.println("\t \t.*' /  .*' ; .*`- +'  `*' ");
         System.out.println("\t \t`*-*   `*-*  `*-*'");
+        //Art by Blazej Kozlowski https://www.asciiart.eu/animals/catsv2
     }
 }
